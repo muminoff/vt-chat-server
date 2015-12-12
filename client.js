a44ba0ccc23630b99e4ae71e5f46c02fa39ec88c
sardor <sardor@insilicogen.com>
2015-12-12 17:14:05 +0900

    Added: triggers for modified field

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('socket.io-client')('http://121.135.130.247:3000');
 socket.on('connect', function(){
   console.log('Connected to socket.io server');
-  socket.emit('signup_request', {'username': 'sardor2', 'phone_number': '998931234589'});
+  socket.emit('signup_request', {'username': 'sardor7', 'phone_number': '998931234594'});
 });
 socket.on('signup_response', function(data){
   console.log(data);
--- sql/yangi.sql
+++ sql/yangi.sql
@@ -16,11 +16,42 @@ create table "users" (
   "auth_token_expires" timestamp without time zone default (current_timestamp at time zone 'utc' + 30 * interval '1 day'),
   "activated" bool default false,
   "code" smallint default generate_random_number(1000, 9999),
-  "imei" character varying(32),
+  "imei" character varying(32) not null unique,
   "is_admin" bool default false,
   "joined" timestamp without time zone default (now() at time zone 'utc'),
-  "updated" timestamp without time zone default (now() at time zone 'utc'),
+  "modified" timestamp without time zone default (now() at time zone 'utc'),
   "photo_url" character varying(255)
 );
 
 create unique index on users(auth_token);
+
+create or replace function set_token_for_user() 
+returns trigger 
+as $$
+begin
+  new.auth_token := case when new.activated = true then md5(random()::text)
+end;
+
+return new;
+end;
+
+$$ language plpgsql;
+
+create trigger set_token_for_user_trigger 
+before update on users 
+for each row 
+  execute procedure set_token_for_user();
+
+create or replace function update_modified_column()	
+returns trigger as $$
+begin
+    new.modified = now() at time zone 'utc';
+    return new;	
+end;
+
+$$ language 'plpgsql';
+
+create trigger update_modified_column_trigger 
+before update on users 
+for each row 
+  execute procedure update_modified_column();
