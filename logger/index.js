cfd2bebdf99e5fe96d42b5f3fd1a7d8bf180536c
muminoff <smuminov@gmail.com>
2015-12-19 17:43:44 +0900

    Implemented: logger handler

--- logger/index.js
+++ logger/index.js
@@ -1,1 +1,31 @@
+var winston = require('winston');
 
+var logger = module.exports = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({
+      filename: './logs/vt.log',
+      json: false,
+      timestamp: function() {
+        return Date();
+      },
+      formatter: function(options) {
+        return options.timestamp() +' - '+ options.level +' - '+ (undefined !== options.message ? options.message : '') +
+          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+      }
+    }),
+    new (winston.transports.Console)({
+      colorize: 'all'
+    }),
+  ]
+});
+
+// logger.level = 'silly';
+// logger.log('silly', JSON.stringify({"foo":"bar"}));
+// logger.log('debug', "127.0.0.1 - there's no place like home");
+// logger.log('verbose', "127.0.0.1 - there's no place like home");
+// logger.log('info', "127.0.0.1 - there's no place like home");
+// logger.log('warn', "127.0.0.1 - there's no place like home");
+// logger.log('error', "127.0.0.1 - there's no place like home");
+// logger.info("127.0.0.1 - there's no place like home");
+// logger.warn("127.0.0.1 - there's no place like home");
+// logger.error("127.0.0.1 - there's no place like home");
--- server.js
+++ server.js
@@ -1,146 +1,26 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var console = require('console');
 var io = require('socket.io')(server);
 var yaml = require('js-yaml');
 var fs = require('fs');
+var logger = require('./logger');
 
 try {
   var config = yaml.safeLoad(fs.readFileSync('conf/config.yml', 'utf8'));
-  console.log("Config opened.");
+  logger.info("Config opened.");
 } catch (e) {
-  console.log(e);
+  console.error(e);
 }
 
 var port = process.env.PORT || config.port;
 var pg = require('pg');
 
 io.on('connection', function (socket) {
-  var addedUser = false;
-  var pgConnString = "postgres://vt:vt@localhost:5432/vt";
-  var doSignupQuery = 'insert into users(username, phone_number) values($1, $2)';
-  var getAuthToken = 'select token from tokens where username=$1';
-  var checkAuthToken = 'select username from tokens where token=$1';
-  var getRoomListQuery = 'select id::int, subject, description, owner, extract(epoch from created_at)::int as created_at from rooms';
-  var getTopicListQuery = 'select id::int, title, body, parent_room::int, owner, archived, extract(epoch from created_at)::int as created_at, attrs from topics where parent_room=$1';
-
-  // signup api
-  socket.on('signup_request', function (data) {
-    console.log(data);
-    var username = data.username;
-    var phone_number = data.phone_number;
-
-    pg.connect(pgConnString, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-
-      client.query(doSignupQuery, [username, phone_number], function(err, result) {
-
-        if(err) {
-          return console.error('error running query', err);
-        }
-
-        client.query(getAuthToken, [username], function(err, result) {
-          if(err) {
-            return console.error('error running query', err);
-          }
-          socket.emit('signup_response', JSON.stringifiy(result.rows[0]));
-
-          done();
-        });
-      });
-    });
-
-  });
-
-  // login api
-  socket.on('login_request', function (data) {
-    console.log(data);
-    var token = data.token;
-
-    pg.connect(pgConnString, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-
-      client.query(checkAuthToken, [token], function(err, result) {
-
-        if(result){
-          console.log(result.rows[0]);
-          socket.emit('login_response', JSON.stringify({'status': 'ok'}));
-        }
-
-        if(err) {
-          return console.error('error running query', err);
-        }
-
-        done();
-
-      });
-    });
-
-  });
-
-  // roomlist api
-  socket.on('roomlist_request', function (data) {
-    console.log(data);
-
-    pg.connect(pgConnString, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-
-      client.query(getRoomListQuery, function(err, result) {
-
-        if(result){
-          console.log(result.rows);
-          socket.emit('roomlist_response', JSON.stringify(result.rows));
-        }
-
-        if(err) {
-          return console.error('error running query', err);
-        }
-
-        done();
-
-      });
-    });
-
-  });
-
-  // roomlist api
-  socket.on('topiclist_request', function (data) {
-    console.log(data);
-    var room_id = data.room_id;
-
-    pg.connect(pgConnString, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-
-      client.query(getTopicListQuery, [room_id], function(err, result) {
-
-        if(result){
-          console.log(result.rows);
-          socket.emit('topiclist_response', JSON.stringify(result.rows));
-        }
-
-        if(err) {
-          return console.error('error running query', err);
-        }
-
-        done();
-
-      });
-    });
-
-  });
-
+  logger.debug("Client connected", type(socket));
 });
 
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+  logger.info('Server listening at port %d', port);
 });
