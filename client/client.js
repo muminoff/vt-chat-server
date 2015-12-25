2cae80142d564f5443c3a4636b3c95a52b7b61f3
sardor <sardor@insilicogen.com>
2015-12-26 01:05:33 +0900

    Added: client changes

--- client/client.js
+++ client/client.js
@@ -1,19 +1,19 @@
-// var socket = require('socket.io-client')('http://121.135.130.247:3000');
+var socket = require('socket.io-client')('http://121.135.130.247:3000');
 // var socket = require('socket.io-client')('http://localhost:3000', {'force new connection': true,   'reconnect': true});
-var socket = require('socket.io-client')('http://localhost:3000');
+// var socket = require('socket.io-client')('http://localhost:3000');
 // var socket = require('socket.io-client')('http://localhost:3000', { query: "token=bar" });
 socket.on('connect', function(){
   // var myToken = 'e92a1e6551f34e1c9a62736d77a5eafc';
   console.log('Connected to socket.io server');
   // socket.emit('authenticate');
-  socket.emit('authenticate', {'token': '37481bc1f8b4443c87b32c9b321f0fad'});
+  // socket.emit('authenticate', {'token': '37481bc1f8b4443c87b32c9b321f0fad'});
   // socket.emit('authenticate', {'token': '886ae2aaf8b44f11acb97163726b7119'});
   // socket.emit('signup_request', {'username': 'bahrom25', 'phone_number': '9981255'});
   // socket.emit('signup_request', {'username': '9981235'});
   // socket.emit('login_request', {'token': myToken });
   // socket.emit('roomlist_request', {'token': myToken });
   // socket.emit('topiclist_request', {'room_id': 1});
-  // socket.emit('signin_request', {'token': 'c328b17d12434fcf9c0c4db2dc02740a'});
+  socket.emit('signin_request', {'token': 'a45dcb642a514a5b92cd149afd9570fa'});
   // setTimeout(function() {
   // socket.emit('topiccreate_request', {'title': 'tttt', 'body': 'tttbody', 'parent_room': 2, 'attrs': null});
   // }, 2000);
@@ -28,7 +28,7 @@ socket.on('connect', function(){
 // socket.on('topiclist_response', function(data){
 // socket.on('signup_response', function(data){
 // socket.on('signin_response', function(data){
-socket.on('authenticate', function(data){
+socket.on('signin_response', function(data){
 // socket.on('signup_response', function(data){
   console.log(data);
 });
