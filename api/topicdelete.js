6926aecfb0a3c4ba4b75c5a6d0f35018b81b816e
sardor <sardor@insilicogen.com>
2015-12-26 01:22:08 +0900

    Added: topic delete api

---
+++ api/topicdelete.js
@@ -0,0 +1,17 @@
+var topicDeleteQuery = 'DELETE FROM topics WHERE topic_id=$1 AND user_id=$2';
+
+var topicDelete = module.exports = function(client, topic_id, owner, logger, callback) {
+
+  // topic create
+  client.query(createTopicQuery, [topic_id, owner, attrs], function(err, result) {
+
+    if(err) {
+      logger.error(err);
+      return callback({ 'status': 'fail', 'detail': err });
+    }
+
+    return callback({ 'status': 'ok' });
+
+  });
+
+}
