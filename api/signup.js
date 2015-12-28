526fb345f362c8fa79764dea28ce028887dcc952
muminoff <smuminov@gmail.com>
2015-12-28 12:35:20 +0500

    Added: GCM token support

--- api/signup.js
+++ api/signup.js
@@ -1,21 +1,20 @@
-// var insertUserQuery = 'INSERT INTO users (username, phone_number) values($1, $2)';
-var insertUserQuery = 'INSERT INTO users (username, phone_number) SELECT CAST($1 AS VARCHAR), CAST($2 AS VARCHAR) WHERE NOT EXISTS (SELECT id FROM users WHERE phone_number=$2)';
-var getTokenQuery = 'SELECT token FROM tokens WHERE user_id=(SELECT id FROM users WHERE phone_number=$2)';
+var insertUserQuery = 'INSERT INTO users (username, phone_number, gcm_token) SELECT CAST($1 AS VARCHAR), CAST($2 AS VARCHAR), CAST($3 AS TEXT) WHERE NOT EXISTS (SELECT id FROM users WHERE phone_number=$2)';
+var getTokenQuery = 'SELECT token FROM tokens WHERE user_id=(SELECT id FROM users WHERE phone_number=$1)';
 
-var signupUser = module.exports = function(client, username, phone_number, logger, callback) {
+var signupUser = module.exports = function(client, username, phone_number, gcm_token, logger, callback) {
 
-  client.query(insertUserQuery, [username, phone_number], function(err, result) {
+  client.query(insertUserQuery, [username, phone_number, gcm_token], function(err, result) {
 
     if(err) {
-      logger.error(err);
+      logger.error("WAS ATTEMPTING TO INSERT USER", err);
     }
 
     logger.debug('Inserting user inside signup api ...');
 
-    client.query(getTokenQuery, [username], function(err, result) {
+    client.query(getTokenQuery, [phone_number], function(err, result) {
 
       if(err) {
-        logger.error(err);
+        logger.error("WAS ATTEMPTING TO GET TOKEN OF USER", err);
       }
 
       logger.debug('Getting token inside signup api ...');
--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -21,7 +21,7 @@ psql -d vt -U vt <utils/schema/subscribers.sql
 psql -d vt -U vt <utils/schema/messages.sql
 
 # VT Robot
-psql -d vt -U vt -c "insert into users (username, phone_number) values('VTRobot', '777')"
+psql -d vt -U vt -c "insert into users (username, phone_number, gcm_token) values('VTRobot', '777', 'vtrobot_gcm_token')"
 
 # Dummy rooms
 psql -d vt -U vt -c "insert into rooms (subject, description, owner) values('Уведомления', 'Важные уведомления', 1), ('SOS', 'Нужна помощь', 1), ('Потеря', 'Ищем документы, и тд.', 1), ('Базар', 'Купля-продажа', 1), ('Днюхи', 'Поздравляем', 1), ('Оффтоп', 'Обо всем', 1)"
