eea3474107573c2644e9333eab0be9b2bd553004
muminoff <smuminov@gmail.com>
2016-01-04 22:11:51 +0900

    Fix: path

--- workers/gcmpushsender.js
+++ workers/gcmpushsender.js
@@ -2,51 +2,51 @@
 var redis = require('redis');
 
 // get main config
-var config = require('./utils/config');
+var config = require('../utils/config');
 
 // set logger
-var logger = require('./logger');
+var logger = require('../logger');
 logger.level = config.log_level;
 
 // redis client instance
-var redisClient = redis.createClient({
-  host: config.redis.host,
-  port: config.redis.port,
-});
+// var redisClient = redis.createClient({
+//   host: config.redis.host,
+//   port: config.redis.port,
+// });
 
-// log error on connection failure
-redisClient.on('error', function(err) {
-  logger.error('Cannot connect to Redis');
-  process.exit(-1);
-});
+// // log error on connection failure
+// redisClient.on('error', function(err) {
+//   logger.error('Cannot connect to Redis');
+//   process.exit(-1);
+// });
 
-// select redis db
-redisClient.select(config.redis.db);
+// // select redis db
+// redisClient.select(config.redis.db);
 
-// if auth set, authenticate redis client
-if(config.redis.auth)redisClient.auth(config.redis.auth);
+// // if auth set, authenticate redis client
+// if(config.redis.auth)redisClient.auth(config.redis.auth);
 
-// log it if connection succeeds
-redisClient.on('connection', function() {
-  logger.info('Connected to Redis');
-});
+// // log it if connection succeeds
+// redisClient.on('connection', function() {
+//   logger.info('Connected to Redis');
+// });
 
-// gcm client
-var gcm = require('node-gcm');
+// // gcm client
+// var gcm = require('node-gcm');
 
-// initialize new gcm message
-var message = new gcm.Message();
+// // initialize new gcm message
+// var message = new gcm.Message();
 
-// add data to message
-message.addData('key1', 'msg1');
+// // add data to message
+// message.addData('key1', 'msg1');
 
-// set up the sender with gcm_api_key
-var sender = new gcm.Sender(config.gcm_api_key);
+// // set up the sender with gcm_api_key
+// var sender = new gcm.Sender(config.gcm_api_key);
 
-var regTokens = redisClient.smembers('topic' + topicid + ':gcm_tokens')
+// var regTokens = redisClient.smembers('topic' + topicid + ':gcm_tokens')
 
-// now the sender can be used to send messages
-sender.send(message, { registrationTokens: regTokens }, function (err, response) {
-    if(err) logger.error(err);
-    else logger.debug(response);
-});
+// // now the sender can be used to send messages
+// sender.send(message, { registrationTokens: regTokens }, function (err, response) {
+//     if(err) logger.error(err);
+//     else logger.debug(response);
+// });
